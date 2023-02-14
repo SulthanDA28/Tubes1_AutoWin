b86330551bd1a4169e9ada3d0b470690f9fc0e0a
Muhammad Habibi Husni <habibihusni18@gmail.com>
2023-02-14 22:01:55 +0700

    feat: added vector method, botservice ambilMakanan method

--- src/main/java/Services/BotService.java
+++ src/main/java/Services/BotService.java
@@ -2,6 +2,7 @@ package Services;
 
 import Enums.*;
 import Models.*;
+import Models.Vector;
 
 import java.util.*;
 import java.util.stream.*;
@@ -34,20 +35,41 @@ public class BotService {
     }
 
     public void computeNextPlayerAction(PlayerAction playerAction) {
-        playerAction.action = PlayerActions.FORWARD;
-        playerAction.heading = new Random().nextInt(360);
+        
+        if (ambilMakanan(playerAction)){
+            System.out.println("Ambil makanan");
+        } else {
+            System.out.println("Random");
+            playerAction.action = PlayerActions.FORWARD;
+            playerAction.heading = new Random().nextInt(360);
 
+        }
+        this.playerAction = playerAction;
+        System.out.println(playerAction.action);
+        System.out.println(playerAction.heading);
+        System.out.println();
+    }
+
+    private boolean ambilMakanan(PlayerAction playerAction){
         if (!gameState.getGameObjects().isEmpty()) {
             var foodList = gameState.getGameObjects()
                     .stream().filter(item -> item.getGameObjectType() == ObjectTypes.FOOD)
                     .sorted(Comparator
                             .comparing(item -> getDistanceBetween(bot, item)))
                     .collect(Collectors.toList());
-
-            playerAction.heading = getHeadingBetween(foodList.get(0));
+            int i = 0;
+            // ambil makanan yang tidak menyebabkan player menyentuh batas arena
+            while (true && i < foodList.size()){
+                double radPlayer = getDistanceBetween(foodList.get(i), new Position(0, 0)) + bot.getSize();
+                if (radPlayer <= getGameState().world.radius){
+                    playerAction.heading = getHeadingBetween(foodList.get(i));
+                    playerAction.action = PlayerActions.FORWARD;
+                    return true;
+                }
+                i++;
+            }
         }
-
-        this.playerAction = playerAction;
+        return false;
     }
 
     public GameState getGameState() {
@@ -70,6 +92,12 @@ public class BotService {
         return Math.sqrt(triangleX * triangleX + triangleY * triangleY);
     }
 
+    private double getDistanceBetween(GameObject object1, Position p) {
+        var triangleX = Math.abs(object1.getPosition().x - p.getX());
+        var triangleY = Math.abs(object1.getPosition().y - p.getY());
+        return Math.sqrt(triangleX * triangleX + triangleY * triangleY);
+    }
+
     private int getHeadingBetween(GameObject otherObject) {
         var direction = toDegrees(Math.atan2(otherObject.getPosition().y - bot.getPosition().y,
                 otherObject.getPosition().x - bot.getPosition().x));
@@ -80,5 +108,31 @@ public class BotService {
         return (int) (v * (180 / Math.PI));
     }
 
+    private boolean willIntersect(GameObject a, GameObject b, Position dest){
+        int B = 1;
+        int A = -(dest.y-a.getPosition().y)/(dest.x-a.getPosition().x);
+        int C = -(a.getPosition().y*A+a.getPosition().x*B);
+        double dist = Math.abs(A*b.getPosition().x + B*b.getPosition().y+C)/Math.sqrt(A*A+B*B);
 
+
+        return Vector.sudut(a.getPosition(),dest,b.getPosition()) <= 90 && 
+                Vector.sudut(dest, a.getPosition(), b.getPosition()) <= 90 &&
+                a.size+b.size < dist;
+    }
+
+    private boolean isObstacle(GameObject a){
+        ObjectTypes objT = a.getGameObjectType();
+        return objT == ObjectTypes.ASTEROIDFIELD || objT == ObjectTypes.GASCLOUD || objT == ObjectTypes.WORMHOLE || objT == ObjectTypes.PLAYER || objT == ObjectTypes.PLAYER;
+    }
+
+    private boolean isFreeObstacle(GameObject a, GameObject b, GameObject obstacle){
+        List<GameObject> gameobj = gameState.getGameObjects();
+        for (int i = 0;i < gameobj.size();i++){
+            if (isObstacle(gameobj.get(i)) &&
+                 willIntersect(bot, gameobj.get(i), b.getPosition())){
+                return false;
+            }
+        }
+        return true;
+    }
 }
